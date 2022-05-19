ac60fb364db6cca3855424b33e603fb4e41cd241
CharlesIoDu <charles.iorioduval@gmail.com>
2022-05-19 11:12:35 -0400

    test

--- monFichier.py
+++ monFichier.py
@@ -1,3 +1,6 @@
 print("Hello world")
 
 #"Le chiffre optimale pour générer des nombres aléatoires c'est 69069 en 32 bits"
+
+
+print("C'est un aveugle qui rentre dans un bar... \n pis dans une table... \n pis dans une chaise....")
