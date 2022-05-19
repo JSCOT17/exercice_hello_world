5589a402e74cc7cdcc0e874d6f97418fb22140db
Jean-Simon Côté <jean-simon.cote.5@ulaval.ca>
2022-05-19 11:19:09 -0400

    "imagine faire un commit pour ça"

--- monFichier.py
+++ monFichier.py
@@ -3,19 +3,4 @@ print("Hello world")
 #"Le chiffre optimale pour générer des nombres aléatoires c'est 69069 en 32 bits"
 
 
-<<<<<<< HEAD
-print("C'est un aveugle qui rentre dans un bar... \n pis dans une table... \n pis dans une chaise....")
-=======
-
-
-
-
-
-
-
-
-
-
-
-print("Monte-Carlo")
->>>>>>> tamere
+print("C'est: un aveugle qui rentre dans un bar... \n pis dans une table... \n pis dans une chaise....")
